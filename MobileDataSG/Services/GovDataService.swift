51eeca355aa88a8da4457711e73691361bdb9e92
Tim Shim <timshim@gmail.com>
2019-08-06 01:01:52 +0800

    Implemented networking request and data mapping to DataRecords in GovDataService

--- MobileDataSG/Services/GovDataService.swift
+++ MobileDataSG/Services/GovDataService.swift
@@ -13,7 +13,7 @@ final class GovDataService {
     enum DataResource: String {
         case mobileDataUsage = "a807b7ab-6cad-4aa6-87d0-e283a7353a0f"
     }
-    typealias RecordsResponse = ([[String: Any]], Error?) -> Void
+    typealias RecordsResponse = ([DataRecord], Error?) -> Void
 
     private static let endpoint = "https://data.gov.sg/api/action/datastore_search"
     private static let offset = 14
@@ -28,6 +28,25 @@ final class GovDataService {
             "fields": fields.joined(separator: ","),
             "limit": "\(limit)",
         ]
+
+        NetworkingService.request(url: url, httpMethod: .GET, params: params) { (json, error) in
+            guard let json = json as? [String: Any] else { return }
+            guard let success = json["success"] as? Bool, success == true else { return }
+            guard let result = json["result"] as? [String: Any] else { return }
+            guard let records = result["records"] as? [[String: Any]] else { return }
+
+            var dataRecords: [DataRecord] = []
+
+            for record in records {
+                guard let quarter = record["quarter"] as? String else { return }
+                guard let volumeString = record["volume_of_mobile_data"] as? String else { return }
+                guard let volume = Double(volumeString) else { return }
+                let dataRecord = DataRecord(quarter: quarter, volume: volume)
+                dataRecords.append(dataRecord)
+            }
+
+            completion(dataRecords, error)
+        }
     }
 
 }
